4fbd9aecef5ee9682562e3a696a529e5660e59a6
daicheez <dai.rotty.2@gmail.com>
2022-01-14 15:17:01 +0900

    ルーティングの再設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   
-  get 'trainings/index'
   root to: 'home#index'
+  get 'trainings/index'
+  get 'trainings/2input'
+  get 'trainings/3reading'
+  get 'trainings/4image_moji_level'
+  get 'trainings/tr_i_m_e1'
+  get 'trainings/tr_i_m_e1_ans'
+  get 'trainings/tr_i_m_e2'
+  get 'trainings/tr_i_m_e3'
+  get 'trainings/tr_i_m_e4'
+  get 'trainings/tr_i_m_e5'
+  get 'trainings/tr_i_m_e6'
+  get 'trainings/tr_i_m_e7'
+  get 'trainings/tr_i_m_e8'
+  get 'trainings/tr_i_m_e9'
+  get 'trainings/tr_i_m_e10'
 end
